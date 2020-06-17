0033d33de61b5961ac68676756ba3acf656f4eb4
jdrake16 <stovetops111@gmail.com>
2020-06-16 20:15:34 -0600

    added draggable button.

--- app/src/main/java/edu/cnm/deepdive/trails/Activity3.java
+++ app/src/main/java/edu/cnm/deepdive/trails/Activity3.java
@@ -1,5 +1,9 @@
 package edu.cnm.deepdive.trails;
 
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,16 @@ public class Activity3 extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_3);
+
+    Button butt6 = findViewById(R.id.butt6);
+
+    butt6.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent int6 = new Intent(Activity3.this,Activity6.class);
+        startActivity(int6);
+      }
+    });
+
   }
 }
