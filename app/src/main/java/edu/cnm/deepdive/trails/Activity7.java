9e898b45b277333aef1f3115b685ca11dde405b8
jdrake16 <stovetops111@gmail.com>
2020-06-21 16:25:33 -0600

    Added image slider.

--- app/src/main/java/edu/cnm/deepdive/trails/Activity7.java
+++ app/src/main/java/edu/cnm/deepdive/trails/Activity7.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.trails;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.viewpager.widget.ViewPager;
 
 public class Activity7 extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Activity7 extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_7);
+
+    ViewPager viewPager = findViewById(R.id.viewPager);
+    ImageAdapter adapter = new ImageAdapter(this);
+    viewPager.setAdapter(adapter);
+
+
   }
 }
