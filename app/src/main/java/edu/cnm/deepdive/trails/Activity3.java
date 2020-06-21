94a8fb8d9dda221378ef146f31edbc3f518a466c
jdrake16 <stovetops111@gmail.com>
2020-06-21 13:25:43 -0600

    Added new button.

--- app/src/main/java/edu/cnm/deepdive/trails/Activity3.java
+++ app/src/main/java/edu/cnm/deepdive/trails/Activity3.java
@@ -15,12 +15,22 @@ public class Activity3 extends AppCompatActivity {
     setContentView(R.layout.activity_3);
 
     Button butt6 = findViewById(R.id.butt6);
+    Button butt7 = findViewById(R.id.butt7);
 
     butt6.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
         Intent int6 = new Intent(Activity3.this,Activity6.class);
         startActivity(int6);
+      }
+    });
+
+
+    butt7.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent int7 = new Intent(Activity3.this,Activity7.class);
+        startActivity(int7);
       }
     });
 
